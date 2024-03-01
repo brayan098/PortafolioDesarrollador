29dbdddb3d3c96ca08025ad4790f51f064d14afb
brayan098 <brayanestivengodoy15@gmail.com>
2024-03-01 07:34:19 -0500

    Mi portafolio =D

---
+++ transicion.js
@@ -0,0 +1,8 @@
+var pepe = document.getElementById("pepe");
+var carga =document.getElementById("barrota");
+function aparecer(){
+    pepe.classList.add("juan")
+    carga.classList.add("barrita")
+}
+
+setTimeout(aparecer, 1500);
